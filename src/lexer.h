239b45960b758e0b83b39c2228f1acc563f2fa7b
if-not-nil <62560250+if-not-nil@users.noreply.github.com>
2025-09-21 16:02:07 +0300

    more lexing

--- src/lexer.h
+++ src/lexer.h
@@ -40,16 +40,61 @@ char *open_file(char *path, int *len) {
 }
 
 INST_TYPE keyword_check_builtin(char *name) {
-  if (strcmp(name, "nop") == 0)
-    return INST_NOP;
   if (strcmp(name, "push") == 0)
     return INST_PUSH;
-  if (strcmp(name, "pop") == 0)
+  else if (strcmp(name, "pop") == 0)
     return INST_POP;
-  if (strcmp(name, "swap") == 0)
+  else if (strcmp(name, "swap") == 0)
     return INST_SWAP;
-  if (strcmp(name, "dup") == 0)
+  else if (strcmp(name, "dup") == 0)
     return INST_DUP;
+  else if (strcmp(name, "nop") == 0)
+    return INST_NOP;
+  else if (strcmp(name, "push") == 0)
+    return INST_PUSH;
+  else if (strcmp(name, "jmp") == 0)
+    return INST_JMP;
+  else if (strcmp(name, "swap") == 0)
+    return INST_SWAP;
+  else if (strcmp(name, "inswap") == 0)
+    return INST_INSWAP;
+  else if (strcmp(name, "indup") == 0)
+    return INST_INDUP;
+  else if (strcmp(name, "print") == 0)
+    return INST_PRINT;
+  else if (strcmp(name, "jmpz") == 0)
+    return INST_JMPZ;
+  else if (strcmp(name, "jpmnz") == 0)
+    return INST_JPMNZ;
+  else if (strcmp(name, "pop") == 0)
+    return INST_POP;
+  else if (strcmp(name, "dup") == 0)
+    return INST_DUP;
+  else if (strcmp(name, "add") == 0)
+    return INST_ADD;
+  else if (strcmp(name, "mod") == 0)
+    return INST_MOD;
+  else if (strcmp(name, "sub") == 0)
+    return INST_SUB;
+  else if (strcmp(name, "mul") == 0)
+    return INST_MUL;
+  else if (strcmp(name, "div") == 0)
+    return INST_DIV;
+  else if (strcmp(name, "cmpe") == 0)
+    return INST_CMPE;
+  else if (strcmp(name, "cmpl") == 0)
+    return INST_CMPL;
+  else if (strcmp(name, "cmpg") == 0)
+    return INST_CMPG;
+  else if (strcmp(name, "cmpge") == 0)
+    return INST_CMPGE;
+  else if (strcmp(name, "cmple") == 0)
+    return INST_CMPLE;
+  else if (strcmp(name, "halt") == 0)
+    return INST_HALT;
+  else if (strcmp(name, "none") == 0)
+    return INST_NONE;
+
   return -1;
 }
 
@@ -72,18 +117,25 @@ int lexer(void) {
   int len = 0;
   char *cur = open_file("./prog.aba", &len);
   int idx = 0;
+
   int pos_line = 0;
   int pos_char = 0;
+
   // printf("%s\n", cur);
   while (idx < len) {
     char c = *(cur + idx);
+
+    if (c == '\n') {
+      pos_line++;
+      pos_char++;
+    }
     if (isalpha(c)) {
       Token tok = keyword_generate(cur, &idx, pos_line, pos_char);
-      printf("tok %s\n", tok.text);
+      printf("tok %s @ %d:%d\n", tok.text, tok.line, tok.character);
     } else if (isdigit(c)) { // idk how it differs from isnumber()
       // printf("%c", c);
-    } else if (c == '\n') {
     }
+
     idx++;
   }
   return 0;
